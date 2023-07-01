45468ff91c4b295cf50c186ad086d468e1971ff1
jitingxu1 <jack9527xu@gmail.com>
2023-07-01 11:07:13 -0700

    setup

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # my-ai-playground
+Record my learning path
---
+++ my-implementation/transformer-from-scratch-pytorch/transformer/encoder.py
@@ -0,0 +1,9 @@
+"""Define encoder"""
+import torch
+import torch.nn as nn
+
+class Encoder(nn.Module):
+    """Encoder with self-attention mechanism"""
+
+    def __init__(self):
+        pass
