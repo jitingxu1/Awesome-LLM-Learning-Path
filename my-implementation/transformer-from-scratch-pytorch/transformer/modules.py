6aa6afcd713219810ddb620a8d2af1b8767470e2
Jiting Xu <126802425+jitingxu1@users.noreply.github.com>
2023-07-02 00:15:38 +0000

    add docstring and notes

--- my-implementation/transformer-from-scratch-pytorch/transformer/modules.py
+++ my-implementation/transformer-from-scratch-pytorch/transformer/modules.py
@@ -20,6 +20,38 @@ class ScaledDotProductAttention(nn.Module):
             value: torch.Tensor, 
             mask: Optional[torch.Tensor] = None
             ):
+        """implements the scaled dot product attention mechanism
+
+        Parameters
+        ----------
+        query ``torch.Tensor``
+            Represents the query tensor, 
+            which has a shape of (batch_size, num_attention_heads, seq_length, hidden_size)
+        key ``torch.Tensor``
+            Represents the key tensor, 
+            which has a shape of (batch_size, num_attention_heads, seq_length, hidden_size)
+        value ``torch.Tensor``
+            Represents the value tensor, 
+            which has a shape of (batch_size, num_attention_heads, seq_length, d_v)
+        mask ``Optional[torch.Tensor]``
+            Represents the mask tensor, 
+            used to mask certain elements in the attention calculation
+            which has a shape of (batch_size, num_attention_heads, seq_length, d_v)
+
+        Return
+        -------
+        values ``torch.Tensor``
+            The contextualized representation obtained by multiplying the attention scores with the value tensor
+            It has the same shape as the value tensor and represents the attended values for each position 
+            in the input sequence. The values tensor has a shape of (batch_size, num_attention_heads, sequence_length, d_v).
+        attentions ``torch.Tensor``
+            Attention scores computed between the query and key tensors
+             It has the same shape as the query and key tensors, 
+             i.e., (batch_size, num_attention_heads, seq_length, seq_length). 
+             The attention scores indicate the importance or weight assigned to 
+             each value vector in the input sequence for each query vector.
+        """
+
 
         # compute the attention score between query and all keys
         # The query and key tensor usually has a shape of 
@@ -34,15 +66,22 @@ class ScaledDotProductAttention(nn.Module):
         # helps stabilize the attention distribution.
         attention = attention / math.sqrt(query.size()[-1])
 
-        # logit transform to (0, 1)
+        #The softmax operation ensures that the attention weights 
+        # are non-negative and sum up to 1, 
+        # indicating the relative importance or contribution 
+        # of each key vector to the corresponding query vector.
         attention = F.softmax(attention, dim=-1)
 
+        # mask certain elements in the attention tensor 
+        # by replacing them with a specific value (-10000).
         if mask:
             attention = attention.masked_fill(mask == 0, -10000)
-        # Weighted sum of value vectors for each input token using attention scores -> 
+
         # new contextualized representation
-        # (batch_size, num_heads, sequence_length, qkv_dim)
-        values = torch.matmul(attention, v)
+        # attention shape: (batch_size, num_attention_heads, sequence_length, sequence_length)
+        # value shape: (batch_size, num_attention_heads, sequence_length, d_v)
+        # values shape: (batch_size, num_attention_heads, sequence_length, d_v)
+        values = torch.matmul(attention, value)
 
         return values, attention
 
